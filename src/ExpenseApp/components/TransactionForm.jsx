35bd8304d2ec5e2acbfb759808f7d987b25dcbfd
szabogerg <szabo.gergo1987@gmail.com>
2025-04-25 13:30:28 +0200

    add: Megjegyzés megadása tranzakciónál

--- src/ExpenseApp/components/TransactionForm.jsx
+++ src/ExpenseApp/components/TransactionForm.jsx
@@ -7,6 +7,7 @@ export default function TransactionForm({ addTransaction }) {
   const [amount, setAmount] = useState("");
   const [type, setType] = useState("income");
   const [date, setDate] = useState(getCurrentDate());
+  const [note, setNote] = useState("");
 
   const handleBtnClick = () => {
     // Ellenőrizzük, hogy a leírás és az összeg meg van-e adva
@@ -18,10 +19,11 @@ export default function TransactionForm({ addTransaction }) {
       description,
       amount: parseInt(amount),
       date: date,
+      note: note,
       type,
     };
 
-/*     console.log(newTransaction); */
+    /*     console.log(newTransaction); */
 
     // Hozzáadjuk az új tranzakciót majd a formot reseteljük
     addTransaction(newTransaction);
@@ -29,21 +31,22 @@ export default function TransactionForm({ addTransaction }) {
     setAmount("");
     setDate(getCurrentDate());
     setType("income");
+    setNote("");
   };
 
   return (
     <div>
+      <input
+        type="text"
+        placeholder="Megnevezés"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+
       <select value={type} onChange={(e) => setType(e.target.value)}>
         <option value="income">Bevétel</option>
         <option value="expense">Kiadás</option>
       </select>
-
-      <input
-        type="text"
-        placeholder="Leírás"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
 
       <input
         type="number"
@@ -57,6 +60,14 @@ export default function TransactionForm({ addTransaction }) {
         value={date}
         onChange={(e) => setDate(e.target.value)}
       />
+
+      <textarea
+        type="text"
+        placeholder="Megjegyzés"
+        value={note}
+        rows={5}
+        onChange={(e) => setNote(e.target.value)}
+      ></textarea>
 
       <button onClick={handleBtnClick}>Hozzáadás</button>
     </div>
