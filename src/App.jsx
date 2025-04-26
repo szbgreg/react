696f692218d7856adb78d7caa86fd8f7361854d4
szbgreg <szbgreg@gmail.com>
2025-04-26 19:30:54 +0200

    update: belépés átállítása react.html-re

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import "./App.css";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/react">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<ClockApp />} />
